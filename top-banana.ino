fb01a7ac8c73218ce7c79f36d1df19dc199c74d2
Glen Pike <github@glenpike.co.uk>
2019-11-08 09:17:00 +0000

    Setup the original animation with addressing, then do test anims.
    The original animation does not seem to work - the entire strip doesn't get
    addressed / lit up at all.  This could be a problem with the number of
    LEDs being incorrect - see previous commit.
    Add in some test animations that use the whole strip to help debug.

--- top-banana.ino
+++ top-banana.ino
@@ -9,12 +9,6 @@
 #include "Single_LED.h"
 #include "Sequence.h"
 
-void SequenceComplete();
-
-#define NEOPIXEL_PIN 4
-#define NEOPIXEL_COUNT 120
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB);
-
 // SINGLE LED PANELS
 #define PANEL_PINK_BALLOON 3
 #define PANEL_ROSETTE 10
@@ -23,7 +17,41 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NEOPIXEL_COUNT, NEOPIXEL_PIN, NEO_GR
 #define PANEL_TRAGIC_SIGN 5
 #define PANEL_THOUGHT_BUBBLE 2
 
-// TODO - rename to match panels.
+// STRIP NeoPixel sections - 1 LED each
+#define PURPLE_PATCH 0
+#define PINK_PATCH 1
+#define YELLOW_BUTTON 2
+#define PINK_BUTTON 3
+#define BLUE_BUTTON 4
+#define POW_BUBBLE_1 5
+#define POW_BUBBLE_2 6
+
+// STRIP NeoPixel sections
+#define BANNER_START 7
+#define BANNER_LENGTH 6
+#define RIBBON_START (BANNER_START + BANNER_LENGTH)
+#define RIBBON_LENGTH 12
+#define BORDER_START (RIBBON_START + RIBBON_LENGTH)
+#define BORDER_LEFT 28
+#define BORDER_TOP 21
+#define BORDER_RIGHT 28
+#define BORDER_BOTTOM 20
+#define BORDER_LENGTH (BORDER_LEFT + BORDER_TOP + BORDER_RIGHT + BORDER_BOTTOM)
+#define BORDER_SPARE 8
+#define HAMMER_START (BORDER_START + BORDER_LENGTH + BORDER_SPARE)
+#define HAMMER_LENGTH 14
+#define FACE_START (HAMMER_START + HAMMER_LENGTH)
+#define FACE_LENGTH 14
+#define TEXT_START (FACE_START + FACE_LENGTH)
+#define TEXT_LENGTH 17
+#define MONEY_START (TEXT_START + TEXT_LENGTH)
+#define MONEY_LENGTH 8
+
+#define NEOPIXEL_PIN 4
+#define NEOPIXEL_COUNT (MONEY_START + MONEY_LENGTH)
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(185, NEOPIXEL_PIN, NEO_GRB);
+
 Single_LED panelPinkBalloon(PANEL_PINK_BALLOON, NULL);
 Single_LED panelRosette(PANEL_ROSETTE, NULL);
 Single_LED panelSpeechBubble(PANEL_SPEECH_BUBBLE, NULL);
@@ -31,21 +59,32 @@ Single_LED panel3Balloons(PANEL_3_BALLOONS, NULL);
 Single_LED panelTragicSign(PANEL_TRAGIC_SIGN, NULL);
 Single_LED panelThoughtBubble(PANEL_THOUGHT_BUBBLE, NULL);
 
-NeoPixel_Section purplePatch(&strip, 0, 1, NULL);
-NeoPixel_Section pinkPatch(&strip, 1, 1, NULL);
-NeoPixel_Section yellowButton(&strip, 2, 1, NULL);
-NeoPixel_Section pinkButton(&strip, 3, 1, NULL);
-NeoPixel_Section blueButton(&strip, 4, 1, NULL);
-NeoPixel_Section powBubble1(&strip, 5, 1, NULL);
-NeoPixel_Section powBubble2(&strip, 6, 1, NULL);
-NeoPixel_Section banner(&strip, 7, 6, NULL);
-NeoPixel_Section ribbon(&strip, 13, 12, NULL);
-NeoPixel_Section border(&strip, 25, 40, NULL);
-NeoPixel_Section hammer(&strip, 65, 10, NULL);
-NeoPixel_Section face(&strip, 75, 10, NULL);
-NeoPixel_Section text(&strip, 85, 10, NULL);
-NeoPixel_Section money(&strip, 95, 8, NULL);
+NeoPixel_Section purplePatch(&strip, PURPLE_PATCH, 1, NULL);
+NeoPixel_Section pinkPatch(&strip, PINK_PATCH, 1, NULL);
+NeoPixel_Section yellowButton(&strip, YELLOW_BUTTON, 1, NULL);
+NeoPixel_Section pinkButton(&strip, PINK_BUTTON, 1, NULL);
+NeoPixel_Section blueButton(&strip, BLUE_BUTTON, 1, NULL);
+NeoPixel_Section powBubble1(&strip, POW_BUBBLE_1, 1, NULL);
+NeoPixel_Section powBubble2(&strip, POW_BUBBLE_2, 1, NULL);
 
+NeoPixel_Section banner(&strip, BANNER_START, BANNER_LENGTH, NULL);
+NeoPixel_Section ribbon(&strip, RIBBON_START, RIBBON_LENGTH, NULL);
+NeoPixel_Section border(&strip, BORDER_START, BORDER_LENGTH, NULL);
+NeoPixel_Section hammer(&strip, HAMMER_START, HAMMER_LENGTH, NULL);
+NeoPixel_Section face(&strip, FACE_START, FACE_LENGTH, NULL);
+NeoPixel_Section text(&strip, TEXT_START, TEXT_LENGTH, NULL);
+NeoPixel_Section money(&strip, MONEY_START, MONEY_LENGTH, NULL);
+NeoPixel_Section everything(&strip, 0, 185, NULL);
+
+AbstractAnimateable* stripAndPanels[] = {
+  &panelThoughtBubble,
+  &panelTragicSign,
+  &panel3Balloons,
+  &panelSpeechBubble,
+  &panelRosette,
+  &panelPinkBalloon,  
+  &everything 
+};
 
 AbstractAnimateable* original[] = {
   &banner, 
@@ -64,16 +103,16 @@ AbstractAnimateable* original[] = {
   &hammer, &face, &text, 
 };
 
-AbstractAnimateable* outsideToCentre[] = {
-  &border,
-  &hammer, &face, &text, &money,
-  &purplePatch, &pinkPatch,
-  &yellowButton, &pinkButton, &blueButton, &powBubble1, &powBubble2,
-  &banner, &ribbon,
-  &panelPinkBalloon, &panelRosette, &panelSpeechBubble, &panel3Balloons, &panelTragicSign, &panelThoughtBubble
-};
+// AbstractAnimateable* outsideToCentre[] = {
+//   &border,
+//   &hammer, &face, &text, &money,
+//   &purplePatch, &pinkPatch,
+//   &yellowButton, &pinkButton, &blueButton, &powBubble1, &powBubble2,
+//   &banner, &ribbon,
+//   &panelPinkBalloon, &panelRosette, &panelSpeechBubble, &panel3Balloons, &panelTragicSign, &panelThoughtBubble
+// };
 
-#define NUM_ANIMATIONS 20
+#define NUM_ANIMATIONS 7
 AnimationConfig ledOn = { ON, 50, 1};
 AnimationConfig animOff = { OFF, 10, 0};
 AnimationConfig whiteWipe = { COLOR_WIPE, 5, NULL, FORWARD, strip.Color(128, 128, 128) };
@@ -223,23 +262,78 @@ AnimationConfig* testAllOff[] = {
   &animOff
 };
 
+AnimationConfig* testEverything[] = {
+  &ledOn,
+  &ledOn,
+  &ledOn,
+  &ledOn,
+  &ledOn,
+  &ledOn,
+  &blueWipe
+};
+AnimationConfig* testRainbow2[] = {
+  &flashYellow,
+  &flashYellow,
+  &flashYellow,
+  &flashYellow,
+  &flashYellow,
+  &flashYellow,
+  &rainbow
+};
+
+
+void SequenceComplete();
+
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
+
 Sequence seq(&SequenceComplete);
 
-#define MAX_STATES 4
+#define MAX_STATES 2
 uint8_t currentState = 0;
 
 void setup() {
 #ifdef SERIAL_DEBUG
     Serial.begin(9600); // We initialize serial connection so that we could
                         // print values from sensor.
-    Serial.println(F("setup "));
+    Serial.print(F("setup with "));
+    Serial.print(NUM_ANIMATIONS);
+    Serial.print(F(" animations "));
+    Serial.print(NEOPIXEL_COUNT);
+    Serial.print(F(" NeoPixels: freeMemory "));
+    Serial.println(freeMemory());
 #endif    
     strip.begin();
     strip.clear();
     strip.show();
+    delay(1000);
+    everything.ColorSet(strip.Color(255, 255, 0));
+    strip.show();
+    delay(1000);
+    everything.ColorSet(strip.Color(0, 255, 255));
+    strip.show();
+    delay(1000);
+    everything.ColorSet(strip.Color(255, 0, 255));
+    strip.show();
+    delay(1000);
 
-    seq.SetAnimateables(original, NUM_ANIMATIONS);
-    seq.SetAnimations(testOriginal, NUM_ANIMATIONS, false);
+    seq.SetAnimateables(stripAndPanels, NUM_ANIMATIONS);
+    seq.SetAnimations(testEverything, NUM_ANIMATIONS, false);
     seq.Start();
     strip.show();
 }
@@ -247,22 +341,12 @@ void setup() {
 void loop() {
     seq.Update();
     strip.show();
-    // delay(1000);
-    // left.ColorSet(strip.Color(255, 255, 0));
-    // strip.show();
-    // delay(1000);
-    // left.ColorSet(strip.Color(0, 255, 255));
-    // strip.show();
-    // delay(1000);
-    // left.ColorSet(strip.Color(255, 0, 255));
-    // strip.show();
-    // delay(1000);
-
 }
 
 void SequenceComplete() {
 #ifdef SERIAL_DEBUG
-    Serial.println(F("SequenceComplete"));
+    Serial.print(F("SequenceComplete freeMemory "));
+    Serial.println(freeMemory());
 #endif
   seq.Pause();
   seq.Reset();
@@ -274,11 +358,11 @@ void SequenceComplete() {
   switch(currentState) {
     case 0:
       delay(1000);
-      seq.SetAnimations(testOriginal, NUM_ANIMATIONS, false);
+      seq.SetAnimations(testEverything, NUM_ANIMATIONS, false);
       break;
     case 1:
       delay(1000);
-      seq.SetAnimations(testAllOff, NUM_ANIMATIONS, false);
+      seq.SetAnimations(testRainbow2, NUM_ANIMATIONS, false);
       break;
     case 2:
       delay(1000);
